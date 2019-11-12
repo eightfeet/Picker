45c9cdfcbda17cac0003a758547228d8ea5eb0cd
eightfeet <bajiao000000@163.com>
2019-11-12 16:48:05 +0800

    update

--- test/getPositionByDefaultValue.test.js
+++ test/getPositionByDefaultValue.test.js
@@ -30,22 +30,56 @@ describe('getPositionByDefaultValue', () => {
 
 	it('When the parameter "data" is a json data, the function returns the correct result', () => {
 		const input = getPositionByDefaultValue(
-			['周日', '09:00'],
+			[0, 9],
 			[
 				{ data: [{ val: 0, date: '周日' }, { val: 1, date: '周一' }, { val: 2, date: '周二' }] },
 				{ data: [{ val: 8, date: '08:00' }, { val: 9, date: '09:00' }, { val: 10, date: '10:00' }] }
 			],
 			{
-				name: 'date',
+				display: 'date',
 				value: 'val'
 			},
 			true,
 			false
 		);
-		const output = [2, 3];
+		const output = [0, 1];
 		expect(input).toStrictEqual(output);
 	});
 
-	
+	it('When the parameter "data" is a cascade data, the function returns the correct result', () => {
+		const input = getPositionByDefaultValue(
+			[10, 2],
+			[
+				{
+					data: [
+						{
+							val: 10,
+							date: '周日',
+							children: [{ val: 1, date: '01:00' }, { val: 2, date: '02:00' }, { val: 3, date: '03:00' }]
+						},
+						{
+							val: 11,
+							date: '周一',
+							children: [{ val: 4, date: '04:00' }, { val: 5, date: '05:00' }, { val: 6, date: '06:00' }]
+						},
+						{
+							val: 12,
+							date: '周二',
+							children: [{ val: 7, date: '07:00' }, { val: 8, date: '08:00' }, { val: 9, date: '09:00' }]
+						}
+					]
+				}
+			],
+			{
+				display: 'date',
+				value: 'val',
+				childs: 'children'
+			},
+			true,
+			true
+		);
+		const output = [0, 1];
+		expect(input).toStrictEqual(output);
+	});
 
 });
